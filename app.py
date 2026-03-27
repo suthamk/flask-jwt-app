818b999f730ae93089ac6eb789aef5de22a121ae
Sutha M <sutha.m@ibm.com>
2026-03-27 17:42:02 +0800

    commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
+from models import bcrypt
 
 app = Flask(__name__)
 
@@ -7,9 +8,14 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "super-secret-key"
 
 jwt = JWTManager(app)
+bcrypt.init_app(app)
 
 from routes.auth import auth_bp
 app.register_blueprint(auth_bp, url_prefix="/auth")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
